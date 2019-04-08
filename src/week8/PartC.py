9460513a635131b55326420c50478d01ac086fac
Norbert Slivka <433308@mail.muni.cz>
2019-04-08 10:38:54 +0200

    w8 init rm partc

--- src/week8/PartC.py
+++
@@ -1,8 +0,0 @@
-from Base import Base, AbstractFilenameProvider
-
-
-class PartC(Base):
-    name = 'C'
-
-    def run(self, fnprovider: AbstractFilenameProvider):
-        return fnprovider.format_files()
--- src/week8/Solution.py
+++ src/week8/Solution.py
@@ -3,6 +3,5 @@ from typing import List
 from Base import Base
 from week8.PartA import PartA
 from week8.PartB import PartB
-from week8.PartC import PartC
 
-SOLUTIONS: List[Base] = [PartA(), PartB(), PartC()]
+SOLUTIONS: List[Base] = [PartA(), PartB()]
